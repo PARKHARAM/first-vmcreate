2bd7d3f98aa0aedb8d128909d7b32c8ae4cd64f8
PARKHARAM <gkfka133@naver.com>
2021-12-21 17:06:45 +0900

    add gcloud

--- instance.tf
+++ instance.tf
@@ -45,7 +45,7 @@ resource "google_compute_instance" "default" {
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    scopes = ["default","storage-rw"]
+    scopes = ["storage-rw"]
   }
 
 
